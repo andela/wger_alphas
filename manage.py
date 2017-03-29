a29e8140ffee1eeaafaba66327be8931841f47fd
Lawrence Wachira <lawrence.wachira@andela.com>
2017-03-29 13:27:04 +0300

    [Chore] Edit manage.py settings path current heroku buildppack path

--- manage.py
+++ manage.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 import sys
-import os
+# import os
 
 from django.core.management import execute_from_command_line
 
-# from tasks import (
-#     setup_django_environment,
-#     get_user_config_path
-# )
+from tasks import (
+    setup_django_environment,
+    get_user_config_path
+)
 
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.settings")
+        # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.settings")
         # Alternative to above
-        # setup_django_environment(get_user_config_path('wger', 'settings.py'))
+        setup_django_environment(get_user_config_path('wger', 'settings.py'))
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
