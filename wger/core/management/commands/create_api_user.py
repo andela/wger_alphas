a5237f6cd681ee1232f522862206e9e56c25efd1
Daniel Wangai <daniel.maina@andela.com>
2017-04-27 16:34:54 +0300

    [Chore #142465509] Remove unnecessary comments.

--- wger/core/management/commands/create_api_user.py
+++ wger/core/management/commands/create_api_user.py
@@ -70,25 +70,3 @@ class Command(BaseCommand):
                 raise CommandError("Incorrect password.")
         else:
             raise CommandError("Username not known.")
-
-
-
-            # # check if user exists
-            # if User.objects.filter(username=options["username"]):
-            #     raise CommandError('Username {} already in use.'.format(options["username"]))
-            # elif User.objects.filter(email=options["email"]):
-            #     raise CommandError("Email {} already in use.".format(options["email"]))
-            # else:
-            #     if creator:
-            #         # create user with default password
-            #         app_user = User.objects.create_user(username=options["username"],
-            #                                             email=options["email"],
-            #                                             password="password123"
-            #                                             )
-            #         app_user.save()
-            #         api_user = ApiUser(user=app_user, created_by=creator[0])
-            #         api_user.save()
-            #         self.stdout.write("API user created.")
-            #     else:
-            #         raise CommandError("There is no creator with username {}. Cannot create new API user.".format(
-            #             options["creator_username"]))
