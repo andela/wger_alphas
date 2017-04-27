72171b4a7842cdebf0292140478efb641ad63a18
Daniel Wangai <daniel.maina@andela.com>
2017-04-27 15:23:05 +0300

    [Feature #142465509] Generate auth token.

--- wger/core/management/commands/create_api_user.py
+++ wger/core/management/commands/create_api_user.py
@@ -1,7 +1,12 @@
+import json
+import requests
+
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 from wger.core.models import ApiUser
+from wger import settings
 
 class Command(BaseCommand):
     '''
@@ -10,29 +15,44 @@ class Command(BaseCommand):
     help = 'create users via API'
 
     def add_arguments(self, parser):
-        # add commandline arguments
+        '''
+        Define commandline arguments to:-
+            authenticate and validate app credentials
+            create new API user
+        '''
         parser.add_argument('username', type=str)
         parser.add_argument('email', type=str)
         parser.add_argument('creator_username', type=str)
+        parser.add_argument('creator_password', type=str)
 
     def handle(self, *args, **options):
-        # check if user exists
-        creator = User.objects.filter(username=options["creator_username"])
-        if User.objects.filter(username=options["username"]):
-            raise CommandError('Username {} already in use.'.format(options["username"]))
-        elif User.objects.filter(email=options["email"]):
-            raise CommandError("Email {} already in use.".format(options["email"]))
-        else:
-            if creator:
-                # create user with default password
-                app_user = User.objects.create_user(username=options["username"],
-                                                    email=options["email"],
-                                                    password="password123"
-                                                    )
-                app_user.save()
-                api_user = ApiUser(user=app_user, created_by=creator[0])
-                api_user.save()
-                self.stdout.write("API user created.")
-            else:
-                raise CommandError("There is no creator with username {}. Cannot create new API user.".format(
-                    options["creator_username"]))
+        # validate APP credentials
+        creator_username = options["creator_username"]
+        creator_password = options["creator_password"]
+        creator = User.objects.filter(username="admin")
+        if creator:
+            # if the creator credentials are valid authenticate
+            response = requests.post(settings.SITE_URL+reverse("rest_auth:rest_login"),
+                                     {"username": creator_username, "password": creator_password})
+            # get auth token from the response
+            auth_token = json.loads(response.content)["key"]
+            self.stdout.write("response {}".format(auth_token))
+        # # check if user exists
+        # if User.objects.filter(username=options["username"]):
+        #     raise CommandError('Username {} already in use.'.format(options["username"]))
+        # elif User.objects.filter(email=options["email"]):
+        #     raise CommandError("Email {} already in use.".format(options["email"]))
+        # else:
+        #     if creator:
+        #         # create user with default password
+        #         app_user = User.objects.create_user(username=options["username"],
+        #                                             email=options["email"],
+        #                                             password="password123"
+        #                                             )
+        #         app_user.save()
+        #         api_user = ApiUser(user=app_user, created_by=creator[0])
+        #         api_user.save()
+        #         self.stdout.write("API user created.")
+        #     else:
+        #         raise CommandError("There is no creator with username {}. Cannot create new API user.".format(
+        #             options["creator_username"]))
