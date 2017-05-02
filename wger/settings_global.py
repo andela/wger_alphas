e50fec065efe505d0311149eda7f8691431a58ae
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-02 15:09:06 +0300

    [Chore] Restore Social login configuratiion settings

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -147,16 +147,16 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # Google plus authentication tokens
-# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['GOOGLE_OAUTH2_KEY']
-# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['GOOGLE_OAUTH2_SECRET']
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['GOOGLE_OAUTH2_KEY']
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['GOOGLE_OAUTH2_SECRET']
 
-# # Twitter authentication tokens
-# SOCIAL_AUTH_TWITTER_KEY = os.environ['TWITTER_KEY']
-# SOCIAL_AUTH_TWITTER_SECRET = os.environ['TWITTER_SECRET']
+# Twitter authentication tokens
+SOCIAL_AUTH_TWITTER_KEY = os.environ['TWITTER_KEY']
+SOCIAL_AUTH_TWITTER_SECRET = os.environ['TWITTER_SECRET']
 
-# # Facebook Authentication tokens
-# SOCIAL_AUTH_FACEBOOK_KEY = os.environ['FACEBOOK_KEY']
-# SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['FACEBOOK_SECRET']
+# Facebook Authentication tokens
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ['FACEBOOK_KEY']
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['FACEBOOK_SECRET']
 
 TEMPLATES = [
     {
